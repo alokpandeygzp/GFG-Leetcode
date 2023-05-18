17f9cc8a32c1c7abdf0d51cb1a8df8cd6e41c41f
Alok Pandey <93366667+alokpandeygzp@users.noreply.github.com>
2023-05-18 08:00:19 +0530

    Added solution - LeetHub

--- Reverse a linked list - GFG/reverse-a-linked-list.cpp
+++ Reverse a linked list - GFG/reverse-a-linked-list.cpp
@@ -35,8 +35,6 @@ class Solution
     //Function to reverse a linked list.
     struct Node* reverseList(struct Node *head)
     {
-        // code here
-        // return head of reversed list
         if (head == NULL||head->next==NULL)
             return head;
 
