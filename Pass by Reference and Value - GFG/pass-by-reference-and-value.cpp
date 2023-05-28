cb12c21086730b286f168d9923e45c2ab73c89ab
Alok Pandey <93366667+alokpandeygzp@users.noreply.github.com>
2023-05-28 16:45:43 +0530

    Added solution - LeetHub

---
+++ Pass by Reference and Value - GFG/pass-by-reference-and-value.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> passedBy(int a, int &b) {
+        vector<int> ans;
+        a=a+1;
+        b=b+2;
+        ans.push_back(a);
+        ans.push_back(b);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        Solution obj;
+        vector<int> ans = obj.passedBy(a, b);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
