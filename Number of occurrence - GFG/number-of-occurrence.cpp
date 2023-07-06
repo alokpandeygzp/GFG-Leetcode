168bcc67413979b0a9365d3d1e1cdcab065eaac8
Alok Pandey <93366667+alokpandeygzp@users.noreply.github.com>
2023-07-06 21:52:45 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -8,53 +8,50 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-	int first(int arr[], int n, int x)
-	{
-	    int low=0,high=n-1;
-	    int ans=-1;
-	    
-	    while(low<=high)
-	    {
-	        int mid=low+(high-low)/2;
-	        
-	        if(arr[mid]==x){
-	            ans=mid;
-	            high=mid-1;
-	        }
-	        else if(arr[mid]<x)
-	            low=mid+1;
-	        else if(arr[mid]>x)
-	            high=mid-1;
-	    }
-	    return ans;
-	}
-	int second(int arr[], int n, int x)
-	{
-	    int low=0,high=n-1;
-	    int ans=-1;
-	    
-	    while(low<=high)
-	    {
-	        int mid=low+(high-low)/2;
-	        
-	        if(arr[mid]==x){
-	            ans=mid;
-	            low=mid+1;
-	        }
-	        else if(arr[mid]<x)
-	            low=mid+1;
-	        else if(arr[mid]>x)
-	            high=mid-1;
-	    }
-	    return ans;
-	}
+    int findLeft(int arr[], int n, int x)
+    {
+        int low=0,high=n-1;
+        int ansLeft=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(arr[mid]==x)
+            {
+                ansLeft=mid;
+                high=mid-1;
+            }
+            else if(arr[mid]<x)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return ansLeft;
+    }
+    int findRight(int arr[], int n, int x)
+    {
+        int low=0,high=n-1;
+        int ansRight=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(arr[mid]==x)
+            {
+                ansRight=mid;
+                low=mid+1;
+            }
+            else if(arr[mid]<x)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return ansRight;
+    }
 	int count(int arr[], int n, int x) {
-	    
-	    int f=first(arr,n,x);
-	    int s=second(arr,n,x);
-	    if(f==-1 && s==-1)
+	    int l=findLeft(arr,n,x);
+	    int r=findRight(arr,n,x);
+	    if(l==-1 && r==-1)
 	        return 0;
-	    return s-f+1;
+	    return r-l+1;
 	}
 };
 
