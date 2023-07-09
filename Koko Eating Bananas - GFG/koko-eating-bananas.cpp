b8a1b29db679613128cd9696b3aff5c52a5e4ea7
Alok Pandey <93366667+alokpandeygzp@users.noreply.github.com>
2023-07-09 23:09:05 +0530

    Added solution - LeetHub

--- Koko Eating Bananas - GFG/koko-eating-bananas.cpp
+++ Koko Eating Bananas - GFG/koko-eating-bananas.cpp
@@ -8,31 +8,33 @@ class Solution {
     bool isValid(vector<int>& piles, int speed, int H) {
         int hours = 0;
         
-        for (int bananas : piles)
-            hours += ceil(static_cast<double>(bananas) / speed);
+        for(int bananas : piles)
+        {
+            hours+=(bananas)/speed;
+            if(bananas%speed!=0)
+                hours++;
+        }
         
-        return hours <= H;
+        return hours<=H;
     }
     int Solve(int n, vector<int>& piles, int h) 
     {
-        int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
-        int result = -1;
+        int low=1;
+        int high=*max_element(piles.begin(), piles.end());
+        int result=-1;
         
-        while (low <= high) 
+        while(low<=high) 
         {
-            int mid = low + (high - low) / 2;
+            int mid=low+(high-low)/2;
             
-            if (isValid(piles, mid, h)) 
+            if(isValid(piles, mid, h)) 
             {
-                result = mid;
-                high = mid - 1;
+                result=mid;
+                high=mid-1;
             } 
             else
-                low = mid + 1;
-            
+                low=mid+1;
         }
-        
         return result;
     }
 };
