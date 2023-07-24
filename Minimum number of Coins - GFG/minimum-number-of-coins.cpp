245e5532fb639d00000b163817da2f3f099eb244
Alok Pandey <93366667+alokpandeygzp@users.noreply.github.com>
2023-07-24 23:03:52 +0530

    Added solution - LeetHub

---
+++ Minimum number of Coins - GFG/minimum-number-of-coins.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> minPartition(int N)
+    {
+        vector<int> coins={2000,500,200,100,50,20,10,5,2,1};
+        
+        int i;
+        for(i=0;i<coins.size();i++)
+            if(coins[i]<=N)
+                break;
+        
+        vector<int> ans;
+        int sum=0;
+        while(i<coins.size())
+        {
+            if(sum+coins[i]<=N)
+            {
+                sum+=coins[i];
+                ans.push_back(coins[i]);
+            }
+            else
+                i++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> numbers = ob.minPartition(N);
+        for(auto u: numbers)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
