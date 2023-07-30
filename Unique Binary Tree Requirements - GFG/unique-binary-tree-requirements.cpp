7f8066047e201e145994aa7b14942c0ecbaf6a25
Alok Pandey <93366667+alokpandeygzp@users.noreply.github.com>
2023-07-30 17:57:53 +0530

    Added solution - LeetHub

---
+++ Unique Binary Tree Requirements - GFG/unique-binary-tree-requirements.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    bool isPossible(int a,int b)
+    {
+        // we would require an Inorder traversal at least once 
+        // and a preorder/postorder traversal at least once.
+        return (a==2 ^ b==2);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b; 
+        cin>>a>>b;
+        Solution obj;
+        bool ans=obj.isPossible(a,b);
+        cout<<ans<<"\n";
+    }
+}
+// } Driver Code Ends
